cfb259385632bb6ca44e02c2c53900c3daeb7be4
ImAL69 <diego_cupitramo@fet.edu.co>
2025-05-13 16:51:33 -0500

    metí cambios en el forms.py

--- drf_simple_crud/registro/forms.py
+++ drf_simple_crud/registro/forms.py
@@ -18,7 +18,7 @@ class RegistroForm(UserCreationForm):
     
     email = forms.EmailField(
         label="Correo Electronico",
-        max_length=30,
+        max_length=40,
         required=True,
         widget=forms.EmailInput(attrs={'placeholder':'Ingresa tu Correo Electrónico'}))
     
@@ -27,6 +27,19 @@ class RegistroForm(UserCreationForm):
         max_length=20,
         required=True,
         widget=forms.TextInput(attrs={'placeholder':'Ingresa tu numero de teléfono'}))
+
+    password1 = forms.CharField(
+        label="contraseña1",
+        max_length=20,
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder':'Ingresa una contraseña'})
+    )
+    password2 = forms.CharField(
+        label="contraseña2",
+        max_length=20,
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder':'Confirme su contraseña'})
+    )
 
     class Meta:
         model = User
@@ -52,7 +65,22 @@ class RegistroForm(UserCreationForm):
         if len(telefono) < 9:
             raise forms.ValidationError("El número de teléfono debe tener al menos 9 dígitos.")
         return telefono
-    
+
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+        if password1 and password1.isdigit():
+            raise forms.ValidationError('La contraseña no puede contener solo números.')
+        if len(password1) < 8:
+            raise forms.ValidationError('La contraseña tiene que tener mínimo 8 caracteres.')
+        return password1 # Asegúrate de devolver el valor limpio
+
+    def clean_password2(self): # Corregido el nombre del metodo
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2: # Asegurarse que password1 y password2 no son None
+         raise forms.ValidationError('Las contraseñas no coinciden.')
+        return password2
+
     def save(self, commit=True):
         user = super().save(commit=False)
         base_username = f"{self.cleaned_data['first_name'].lower()}_{self.cleaned_data['last_name'].lower()}"
@@ -75,4 +103,4 @@ class RegistroForm(UserCreationForm):
             perfil.telefono = self.cleaned_data['telefono']
             perfil.save()
             
-            return user
+        return user
